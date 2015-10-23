54039dd8fdef33d771d15d2302328df7f32e6164
ffwilliam1992 <1198223539@qq.com>
2015-10-23 10:45:54 +0800

    changed!

---
+++ src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+    	if(s=="")
+    		return 0;
+    	int []hash=new int [256];
+    	Arrays.fill(hash, -1);
+    	int start=0,ans=0;
+
+    	return 0;
+    }
+    public static void main(String []args){
+    	System.out.println("Hello,world!");
+
+    }
+}
